95f737a4e298f0995bd7671cb914321b64470398
Sujay Bhowmick <sujaybhowmick@gmail.com>
2014-08-03 11:44:00 +0530

    moved phone config to phoneModule.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,34 +11,12 @@
 var app = angular.module('phonecatv2App', ['ngResource', 'ui.router', 
                 'phones.phoneModule', 'common.commonModule']);
 
+app.run(
+  [ '$rootScope', '$state', '$stateParams',
+    function ($rootScope, $state, $stateParams) {
 
-app.config(['$stateProvider', '$urlRouterProvider',
-    function($stateProvider, $urlRouterProvider){
-        $urlRouterProvider.otherwise('/phones');
-
-        $stateProvider.state('phones',{
-            abstract: true,
-            url: '/phones',
-            templateUrl: 'views/phones/phones.html',
-            controller: 'phoneController',
-            resolve: {
-                phones: function(phoneModel){
-                    return phoneModel.loadPhones();
-                }
-            }
-        }).state('phones.list', {
-            url: '',
-            templateUrl: 'views/phones/phones.list.html'
-        }).state('phones.detail', {
-            url:'/{id}',
-            views:{
-                '':{
-                    controller: 'phoneDetailController',
-                    templateUrl: '/views/phones/phones.detail.html'
-                }
-            }
-            
-            
-        });
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
     }
-]);
+  ]
+);
--- app/scripts/phones/PhoneModule.js
+++ app/scripts/phones/PhoneModule.js
@@ -1,6 +1,37 @@
 
     var phoneModule = angular.module('phones.phoneModule', []);
 
+    phoneModule.config(['$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider){
+        $urlRouterProvider.otherwise('/phones');
+
+        $stateProvider.state('phones',{
+            abstract: true,
+            url: '/phones',
+            templateUrl: 'views/phones/phones.html',
+            controller: 'phoneController',
+            resolve: {
+                phones: function(phoneModel){
+                    return phoneModel.loadPhones();
+                }
+            }
+        }).state('phones.list', {
+            url: '',
+            templateUrl: 'views/phones/phones.list.html'
+        }).state('phones.detail', {
+            url:'/{id}',
+            views:{
+                '':{
+                    controller: 'phoneDetailController',
+                    templateUrl: '/views/phones/phones.detail.html'
+                }
+            }
+            
+            
+        });
+    }
+]);
+
 
     phoneModule.service('phoneResource', ['$resource', 'configuration', 
         function($resource, configuration){
